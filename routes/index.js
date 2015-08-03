0a2c538907091cd7362b973ab0f029d5d71bc540
Zizox <zizou.arion@gmail.com>
2015-08-03 13:04:15 +0100

    Entrega p2p 8

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,29 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
-});
+// Página de entrada (home page)
+ router.get('/', function(req, res) {
+   res.render('index', { title: 'Quiz', errors: []});
+ });
 
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);  // autoload :quizId
+
+// Definición de rutas de /quizes
+router.get('/quizes/',                     quizController.index);
 router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
-router.get('/credits/author', quizController.author);
+router.get('/quizes/answer',     quizController.answer);
+router.get('/credits/author',    quizController.author);
+router.get('/quizes/:quizId(\\d+)',        quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/new',                  quizController.new);
+router.post('/quizes/create',              quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit',   quizController.edit);
+router.put('/quizes/:quizId(\\d+)',        quizController.update);
+
+
+router.get('/author', function(req, res){
+	res.render('author');
+});
 
 module.exports = router;
